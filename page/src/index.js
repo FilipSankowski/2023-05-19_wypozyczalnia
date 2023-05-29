a0a209d22bd36b9228dbaec145dc3140cc1ab7ec
Royalo04 <filip.sankowski@gmail.com>
2023-05-29 22:08:31 +0200

    removed redundant index.css file

--- page/src/index.js
+++ page/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
+
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import Error from './pages/Error';
